0b5b361aaf681926b4df719c9fa25daa4d7b5cd3
Zac Jones <zacjones93@gmail.com>
2018-06-21 14:50:32 -0700

    added dart file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # egghead-dart
+
+## Installation
+ - [Install Dart](https://webdev.dartlang.org/guides/get-started#2-install-dart)
+ - Run:
+```bash
+$ dart main.dart
+```
---
+++ main.dart
@@ -0,0 +1,10 @@
+import "dart:io";
+
+void main() {
+  HttpServer.bind("localhost", 8080).then((HttpServer server) {
+    server.listen((HttpRequest request) {
+      request.response.write("Hello world");
+      request.response.close();
+    });
+  });
+}
