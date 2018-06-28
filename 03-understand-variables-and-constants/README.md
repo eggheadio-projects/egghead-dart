bd72adbee2d0c3ac2eb04fe08e0f35265436ff76
Jermaine Oppong <creativebracket@gmail.com>
2018-06-28 20:17:53 +0100

    Adds lesson code for variables and constants.

---
+++ 03-understand-variables-and-constants/README.md
@@ -0,0 +1,10 @@
+# Understand Variables and Constants in Dart
+
+## Installation
+
+- [Install Dart](https://www.dartlang.org/tools/sdk#install)
+- Run:
+
+```bash
+$ dart bin/main.dart
+```
---
+++ 03-understand-variables-and-constants/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  var a = 1;
+  print(a);
+
+  int b = 2;
+  print(b);
+
+  final c = 'Hello';
+  // c = 'Hello again'; // Uncomment to throw
+  print(c);
+
+  const d = 'World';
+  print(d);
+}
